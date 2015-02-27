53754e4ccb3d7993607fe4d2985bdfcffe3cae14
Jim Yuan <aquarior@gmail.com>
2015-02-27 15:40:16 +0800

    gh-pages: new blog added, lab folder for testing

--- _sass/config.rb
+++ _sass/config.rb
@@ -1,9 +1,10 @@
-#require 'jekyll-compass'
-#project_type = :jekyll
+Encoding.default_external = 'utf-8'
+
 http_path = "/"
 sass_dir = "./"
 css_dir = "../css"
 images_dir = "../img"
 javascripts_dir = "../js"
 relative_assets = true
-output_style = :compressed
+output_style = :expanded
+#output_style = :compressed
